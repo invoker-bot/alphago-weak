bdc3690ba4465b0ba2eacece347f8b49d608411d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 04:06:52 +0000

    Adding tarfile member sanitization to extractall()

--- alphago_weak/dataset/basic.py
+++ alphago_weak/dataset/basic.py
@@ -99,7 +99,29 @@ class GameArchive(object):
         tmp_path = dest_path + ".tmp"
         if force or not path.exists(dest_path):
             with tarfile.open(archive) as a:
-                a.extractall(tmp_path)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(a, tmp_path)
                 rename(tmp_path, dest_path)
 
     def unpack(self, force=False):
